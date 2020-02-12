21d5bbba288596d389bc9456ac1a3c418aff208b
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-12 05:05:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,16 +11,15 @@ def roster
   @roster
 end
 
+def add_student(name, grade)
+  roster[grade] = []
+  roster[grade] << name
+end 
+
+
 def grade(grade)
   @roster[grade]
 end
 
-
-
-def add_student(name, grade)
-  roster[grade] = []
-  roster[grade] << name
-  
-end 
 
   
